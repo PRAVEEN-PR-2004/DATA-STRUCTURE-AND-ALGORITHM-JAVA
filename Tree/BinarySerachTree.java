7b90eba2cb5bb241e4ea5b3597bc4291ea1225b8
PRAVEEN-PR-2004 <praveen2004ttp@gmail.com>
2024-09-06 11:45:46 +0530

    start

--- Tree/BinarySerachTree.java
+++ Tree/BinarySerachTree.java
@@ -16,8 +16,106 @@ public class BinarySerachTree {
     {
         root = new Node(val);
     }
+    public static Node insert(Node root,int val)
+    {
+        if(root==null)
+        {
+            return new Node(val);
+        }
+        if(root.key<val)
+        {
+           root.right =  insert(root.right,val);
+        }
+        else
+        {
+            root.left = insert(root.left, val);
+        }
+        return root;
+    }
+    public static void Inorder(Node root)
+    {
+        if(root!=null)
+        {
+            Inorder(root.left);
+            System.out.print(root.key+" ");
+            Inorder(root.right);
+        }
+    }
+    public static Node search(Node root,int val)
+    {
+        if(root==null || root.key == val)
+        {
+            return root;
+        }
+        if(val<root.key)
+        {
+            return search(root.left,val);
+        }
+        else
+        {
+           return search(root.right, val);
+        }
+        
+    }
+    public static Node delete(Node root,int val)
+    {
+        if(root==null)
+        {
+            return root;
+        }
+        if(root.key<val)
+        {
+            root.right= delete(root.right, val);
+        }
+        else if(root.key>val)
+        {
+            root.left=delete(root.left,val);
+        }
+        else
+        {
+            if(root.right==null)
+            {
+                return root.left;
+            }
+            else if(root.left==null)
+            {
+
+                return root.right;
+            }
+            root.key = min(root.right);
+            root.right = delete(root.right, root.key);
+        }
+        return root;
+    }
+    public static int min(Node root)
+    {
+        int minval = root.key;
+        while(root.left!=null)
+        {
+            minval = root.left.key;
+            root = root.left;
+        }
+        return minval;
+    }
     public static void main(String[] args) {
-        BinarySerachTree bst = new BinarySerachTree(10);
+        BinarySerachTree bst = new BinarySerachTree(20);
+        insert(bst.root, 70);
+        insert(bst.root, 10);
+        insert(bst.root, 25);
+        insert(bst.root, 60);
+        insert(bst.root, 90);
+        if(search(bst.root,61)==null)
+        {
+            System.out.println("not found");
+        }
+        else
+        {
+            System.out.println("found");
+        }
+        delete(bst.root,25);
+        Inorder(bst.root);
+        
+
     }
     
 }
